93f3c43d7ac3137c43542c11cb47fa1a1c9bd628
Pikj [JReydman] Reyderman <58705342+jreydman@users.noreply.github.com>
2023-10-11 00:38:54 +0300

    fix: auth navigation

--- lib/shared/screens/authorization/authorization.screen.dart
+++ lib/shared/screens/authorization/authorization.screen.dart
@@ -14,20 +14,27 @@ class AuthorizationScreen extends StatefulWidget {
 }
 
 class AuthorizationScreenState extends State<AuthorizationScreen> {
-
+  
   int _pageViewIndex=0;
   final PageController _pgController = PageController(initialPage: 0, keepPage: true);
   List pageContent = <Widget>[const Signin(), const Signup()];
+
+  void openPage(int index) {
+    setState(()=>_pageViewIndex=index);
+    _pgController.animateToPage(_pageViewIndex, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+    GetIt.I<Talker>().debug('${toString()}: open ${pageContent[_pageViewIndex].toString()} page');
+  }
   
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children:[
-        InkResponse(onTap: () => setState(() {_pageViewIndex = 0; GetIt.I<Talker>().debug('value='+_pageViewIndex.toString()); }), child: const Text("Signin")),
-        Text('Authorization'),
-        InkResponse(onTap: () => setState(() {_pageViewIndex = 1; GetIt.I<Talker>().debug('value='+_pageViewIndex.toString()); }), child: const Text("Signup"))
+        InkResponse(onTap: () => openPage(0), child: const Text("Signin")),
+        const Text('Authorization'),
+        InkResponse(onTap: () => openPage(1), child: const Text("Signup"))
     ])),
     drawer: const T8sNavbar(),
     body: PageView.builder(
+        onPageChanged: (index)=>setState(() => _pageViewIndex=index),
         controller: _pgController,
         itemCount: pageContent.length,
         itemBuilder: (context, pageViewIndex) {return pageContent[pageViewIndex];}
